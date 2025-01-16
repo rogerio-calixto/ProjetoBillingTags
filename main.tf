b5027c9ce7c96f871b5e0a297637abc3f12f98ef
Rogerio Calixto.futura@gmail.com <rogerio.calixto@futurasistemas.com.br>
2025-01-16 13:15:23 -0300

    ajuste default_tags

--- main.tf
+++ main.tf
@@ -7,6 +7,14 @@ provider "aws" {
 
   # default_tags {
   #   tags = {
+  #     Ambiente     = "Ambiente_Demonstracao"
+  #     Projeto      = "Projeto_Demonstracao"
+  #     Tipo_Criacao = "Tipo_Demonstracao"
+  #   }
+  # }
+
+  # default_tags {
+  #   tags = {
   #     Ambiente     = var.ambiente
   #     Projeto      = local.projeto
   #     Tipo_Criacao = local.tipo_criacao
